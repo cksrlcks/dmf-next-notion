80b2cfc7eda85021dc20366f6671599e160a8a24
chanki <chanki.kim89@gmail.com>
2022-11-05 23:25:24 +0900

    favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/reset.css";
 import "../styles/common.css";
 import RootLayout from "./layout";
 import { useEffect, useState } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
     const [autoTime, setAutoTime] = useState(2000);
@@ -14,9 +15,31 @@ function MyApp({ Component, pageProps }) {
         setAutoTime(initalTime);
     }, []);
     return (
-        <RootLayout>
-            <Component {...pageProps} autoTime={autoTime} changeSlideShowTime={changeSlideShowTime} />
-        </RootLayout>
+        <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+                <title>디어마이프렌즈</title>
+                <link rel="apple-touch-icon" sizes="57x57" href="/favicon/apple-icon-57x57.png" />
+                <link rel="apple-touch-icon" sizes="60x60" href="/favicon/apple-icon-60x60.png" />
+                <link rel="apple-touch-icon" sizes="72x72" href="/favicon/apple-icon-72x72.png" />
+                <link rel="apple-touch-icon" sizes="76x76" href="/favicon/apple-icon-76x76.png" />
+                <link rel="apple-touch-icon" sizes="114x114" href="/favicon/apple-icon-114x114.png" />
+                <link rel="apple-touch-icon" sizes="120x120" href="/favicon/apple-icon-120x120.png" />
+                <link rel="apple-touch-icon" sizes="144x144" href="/favicon/apple-icon-144x144.png" />
+                <link rel="apple-touch-icon" sizes="152x152" href="/favicon/apple-icon-152x152.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon-180x180.png" />
+                <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-icon-192x192.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="96x96" href="/favicon/favicon-96x96.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+                <link rel="shortcut icon" href="/favicon/favicon.ico" />
+                <link rel="manifest" href="/favicon/manifest.json" />
+                <meta name="theme-color" content="#ffffff"></meta>
+            </Head>
+            <RootLayout>
+                <Component {...pageProps} autoTime={autoTime} changeSlideShowTime={changeSlideShowTime} />
+            </RootLayout>
+        </>
     );
 }
 
