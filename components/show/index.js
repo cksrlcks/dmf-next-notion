7f729da52f373c16d40d0b29b3a900aa78e061b3
chanki.kim <chanki.kim89@gmail.com>
2022-11-09 18:07:58 +0900

    쇼슬라이드 수정

--- components/show/index.js
+++ components/show/index.js
@@ -13,7 +13,7 @@ export default function Show({ menu, autoTime }) {
     return (
         <div className={styles.slideWrapper}>
             <Swiper modules={[Autoplay, EffectFade]} autoplay={{ delay: autoTime }} effect={"fade"} allowTouchMove={false}>
-                {showItems.map((item) => (
+                {shuffle(showItems).map((item) => (
                     <SwiperSlide key={item.id} className={styles["swiper-slide"]}>
                         <Thumbnail url={item.properties.cover_img.files[0]?.name} size={"c_fill,h_700,w_700"} />
                         <Thumbnail url={item.properties.cover_img.files[0]?.name} size={"c_fill,h_700,w_700"} />
