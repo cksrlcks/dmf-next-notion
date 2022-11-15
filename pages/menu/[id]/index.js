1ee9b40f447e346a9e014ae2bf340a472f34425c
chanki <chanki.kim89@gmail.com>
2022-11-15 21:09:19 +0900

    getserversideprops 제거 및 useRouter로 클라이언트에서 path가져옴

--- pages/menu/[id]/index.js
+++ pages/menu/[id]/index.js
@@ -1,17 +1,14 @@
+import { useRouter } from "next/router";
 import useDatabase from "../../../hook/useDatabase";
 import Detail from "../../../components/menuDetail";
 
-export default function DetailView({ params }) {
-    const { id } = params;
+export default function DetailView() {
+    const router = useRouter();
+    const { id } = router.query;
     const { data, error } = useDatabase(`/api/menu/${id.replace(/\-/g, "")}`);
 
     if (error) return <div className="super-loading">데이터를 가져오는데 실패했습니다.</div>;
     if (!data) return <div className="super-loading">정보를 가져오고 있습니다.</div>;
 
     return <Detail item={data[0]} />;
-}
-export function getServerSideProps(context) {
-    return {
-        props: { params: context.params },
-    };
 }
