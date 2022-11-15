25e13994a65c49170c3b5bd3693f5b38d108d83b
chanki <chanki.kim89@gmail.com>
2022-11-15 21:13:31 +0900

    수정

--- pages/menu/[id]/index.js
+++ pages/menu/[id]/index.js
@@ -5,7 +5,8 @@ import Detail from "../../../components/menuDetail";
 export default function DetailView() {
     const router = useRouter();
     const { id } = router.query;
-    const { data, error } = useDatabase(`/api/menu/${id.replace(/\-/g, "")}`);
+    const urlId = id?.replace(/\-/g, "");
+    const { data, error } = useDatabase(`/api/menu/${urlId}`);
 
     if (error) return <div className="super-loading">데이터를 가져오는데 실패했습니다.</div>;
     if (!data) return <div className="super-loading">정보를 가져오고 있습니다.</div>;
