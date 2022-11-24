c26eab93da4e9cfdedd391f034eadd61ec896059
chanki <chanki.kim89@gmail.com>
2022-11-24 22:45:45 +0900

    필터 수정

--- components/menuList/index.js
+++ components/menuList/index.js
@@ -3,25 +3,23 @@ import Tab from "./tab";
 import MenuItem from "../menuItem";
 import styles from "./style.module.css";
 
+const filters = ["전체", "뼈간식", "건조간식", "화식", "베이커리"];
+const getFilteredLists = (originList, filterName) => {
+    if (filterName === "전체") {
+        return originList;
+    }
+    return originList.filter((item) => item.properties.category.select?.name === filterName);
+};
+
 export default function MenuList({ data }) {
-    const [menus, setMenus] = useState(data);
-    const [currentTab, setCurrentTab] = useState(0);
+    const [filter, setFilter] = useState(filters[0]);
+    const filtered = getFilteredLists(data, filter);
 
-    const changeCategory = function (name, num) {
-        if (name.length) {
-            const filterdList = data.filter((item) => item.properties.category.select?.name === name);
-            setMenus((prev) => filterdList);
-        } else {
-            setMenus((prev) => data);
-        }
-
-        setCurrentTab(num);
-    };
     return (
         <div>
-            <Tab styles={styles} handleTab={changeCategory} currentTab={currentTab} data={data} />
+            <Tab styles={styles} filters={filters} filter={filter} onFilterChange={setFilter} data={data} />
             <ul className={styles["menu-list"]}>
-                {menus.map((item) => {
+                {filtered.map((item) => {
                     return <MenuItem product={item} key={item.id} />;
                 })}
             </ul>
